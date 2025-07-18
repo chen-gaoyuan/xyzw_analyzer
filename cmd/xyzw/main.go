a387d06ca95d0533a2837666d0fb708aa3f000b0
DESKTOP-U74DDVQ\Coder <coder@qq.com>
2025-07-18 20:24:56 +0800

    关闭更新检测

--- cmd/xyzw/main.go
+++ cmd/xyzw/main.go
@@ -144,12 +144,12 @@ func main() {
 	// 显示免责声明
 	showDisclaimer()
 
-	// 检查更新，如果有新版本则退出程序
-	if checkForUpdates() {
-		fmt.Println()
-		waitForExit()
-		return
-	}
+	// // 检查更新，如果有新版本则退出程序
+	// if checkForUpdates() {
+	// 	fmt.Println()
+	// 	waitForExit()
+	// 	return
+	// }
 
 	// 设置代理
 	err := gosysproxy.SetGlobalProxy("127.0.0.1:12311", "localhost;127.*;10.*;172.16.*;172.17.*;172.18.*;172.19.*;172.20.*;172.21.*;172.22.*;172.23.*;172.24.*;172.25.*;172.26.*;172.27.*;172.28.*;172.29.*;172.30.*;172.31.*;192.168.*")
